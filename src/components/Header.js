c4256a6c34c72447f4b6bcf2c8d5221cb367a903
Mingcheng lin <mingchenglin2@gmail.com>
2021-12-25 18:30:19 -0600

    added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,35 @@
 import React from 'react'
 import "./Header.css"
+import MenuIcon from '@mui/icons-material/Menu';
+import { Avatar,IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import AppsIcon from '@mui/icons-material/Apps';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 function Header() {
     return (
         <div className="header">
-            <h2>This is a header</h2>
+            <div className="header_left">
+                   <IconButton>
+                      <MenuIcon/>
+                    </IconButton>
+                    <img src="https://cdn.vox-cdn.com/thumbor/K-q2WRPRyxxzzPLjxHGt26swMfM=/0x0:1320x880/1200x800/filters:focal(555x335:765x545)/cdn.vox-cdn.com/uploads/chorus_image/image/67587450/newgmaillogo.0.jpg" alt=""/>
+            </div>
+
+            <div className="header_middle">
+              <SearchIcon/>
+              <input placeholder="Search mail" type="text"/>
+              <ArrowDropDownIcon className="header_inputCaret"/>
+            </div> 
+            <div className="header_right">
+              <IconButton>
+                  <AppsIcon/>
+              </IconButton>
+              <IconButton>
+                  <NotificationsIcon/>
+              </IconButton>
+              <Avatar/>
+            </div>  
         </div>
     )
 }
