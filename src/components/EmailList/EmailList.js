85b01ed143fcab7c6979ccf4e69cbdadee192d7d
Mingcheng lin <mingchenglin2@gmail.com>
2022-01-11 19:06:38 -0600

    added

--- src/components/EmailList/EmailList.js
+++ src/components/EmailList/EmailList.js
@@ -1,14 +1,50 @@
 import React from 'react'
 import './Emailist.css'
-import { Checkbox } from '@mui/material';
+import { Checkbox,IconButton } from '@mui/material';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import RedoIcon from '@mui/icons-material/Redo';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import KeyboardHideIcon from '@mui/icons-material/KeyboardHide';
+import SettingsIcon from '@mui/icons-material/Settings';
 function EmailList() {
     return (
         <div className="emailList">
-            <div className="emailList-settings">
+            <div className="emailList_settings">
             <div className="emailList_settingsLeft">
                   <Checkbox/>
+                  <IconButton>
+                    <ArrowDownwardIcon/>
+                  </IconButton>
+                  <IconButton>
+                    <RedoIcon/>
+                  </IconButton>
+                  <IconButton>
+                    <MoreVertIcon/>
+                  </IconButton>
+            </div>
+            <div className="emailList_settingsRight">
+                  <IconButton>
+                    <ChevronLeftIcon/>
+                  </IconButton>
+                  <IconButton>
+                   <ChevronRightIcon/>
+                  </IconButton>
+                  <IconButton>
+                    <KeyboardHideIcon/>
+                  </IconButton>
+                  <IconButton>
+                  <SettingsIcon/>
+                   </IconButton>
             </div>
             </div>
+
+            <div className="emailList_sections">
+
+            </div>
+
+
         </div>
     )
 }
