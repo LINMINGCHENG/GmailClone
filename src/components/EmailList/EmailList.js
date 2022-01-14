4c11bfb8dfc5319fe2107624a74a0c475d584bba
McJohnsonLin <48139258+LINMINGCHENG@users.noreply.github.com>
2022-01-13 21:07:54 -0500

    added

--- src/components/EmailList/EmailList.js
+++ src/components/EmailList/EmailList.js
@@ -8,6 +8,11 @@ import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import KeyboardHideIcon from '@mui/icons-material/KeyboardHide';
 import SettingsIcon from '@mui/icons-material/Settings';
+import Section from '../Section/Section';
+import InboxIcon from '@mui/icons-material/Inbox';
+import PeopleIcon from '@mui/icons-material/People';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import EmailRow from './EmailRow/EmailRow';
 function EmailList() {
     return (
         <div className="emailList">
@@ -41,9 +46,25 @@ function EmailList() {
             </div>
 
             <div className="emailList_sections">
-                <Setion Icon={InboxIcon}
+             <Section Icon={InboxIcon} title="primary" color="red" selected/>
+             <Section Icon={PeopleIcon} title="Social" color="#1A73E8" />
+             <Section Icon={LocalOfferIcon} title="Promotions" color="green"/>
             </div>
-
+            
+             <div className="emailList_list">
+                <EmailRow
+                 title="Twitch"
+                 subject="Hey fellow streamer!!!"
+                 description="This is a text message"
+                 time="10pm"
+                 />
+                 <EmailRow
+                 title="Twitch"
+                 subject="Hey fellow streamer!!!"
+                 description="This is a text message"
+                 time="10pm"
+                />
+             </div>
 
         </div>
     )
--- src/components/Mail/Mail.js
+++ src/components/Mail/Mail.js
@@ -1,9 +1,82 @@
 import React from 'react'
+import "./Mail.css"
+import IconButton from '@mui/material/IconButton';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import MoveToInboxIcon from '@mui/icons-material/MoveToInbox';
+import ErrorIcon from '@mui/icons-material/Error';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EmailIcon from '@mui/icons-material/Email';
+import WatchLaterIcon from '@mui/icons-material/WatchLater';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import LabelImportantIcon from '@mui/icons-material/LabelImportant';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import { useNavigate } from 'react-router-dom';
+import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
+import PrintIcon from '@mui/icons-material/Print';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+
 
 function Mail() {
+
+     const navigate = useNavigate ();
     return (
-        <div>
-            
+        <div className="mail">
+            <div className="mail_tools">
+                <div className="mail_toolsLeft">
+                        <IconButton onClick={()=>navigate("/")}>
+                          <ArrowBackIcon/>
+                        </IconButton>
+                       <IconButton>
+                          <MoveToInboxIcon/>
+                        </IconButton>
+                        <IconButton>
+                           <ErrorIcon/>
+                        </IconButton>
+                       <IconButton>
+                         <DeleteIcon/>
+                    </IconButton>
+                    <IconButton>
+                           <EmailIcon/>
+                        </IconButton>
+                       <IconButton>
+                         <WatchLaterIcon/>
+                    </IconButton>
+                     <IconButton>
+                         <CheckCircleIcon/>
+                    </IconButton>
+                    <IconButton>
+                           <LabelImportantIcon/>
+                    </IconButton>
+                    <IconButton>
+                         <MoreVertIcon/>
+                    </IconButton>
+                </div>
+                <div className="mail_toolsRight">
+                    <IconButton>
+                         <UnfoldMoreIcon/>
+                    </IconButton>
+                    <IconButton>
+                           <PrintIcon/>
+                    </IconButton>
+                    <IconButton>
+                          <ExitToAppIcon/>
+                    </IconButton>
+                </div>
+            </div>
+            <div className="mail_body">
+                <div className="mail_bodyHeader">
+                    <h2>subject</h2>
+                    <LabelImportantIcon className="mail_important" />
+                    <p>Title</p>
+                    <p className="mail_time">10pm</p>
+                </div>
+                
+                <div className="mail_message">
+                    <p>This is a message </p>
+                </div>
+
+             </div>
+
         </div>
     )
 }
